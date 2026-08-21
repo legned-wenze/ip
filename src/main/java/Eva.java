7ecc433717985f4bfecb08e736ed98e987b7b115
lwz <lwz@123.com>
2026-08-21 14:24:25 +0800

    Level-3

--- src/main/java/Eva.java
+++ src/main/java/Eva.java
@@ -4,7 +4,7 @@ public class Eva {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         System.out.println("Hello! I'm Eva.");
@@ -20,8 +20,20 @@ public class Eva {
                 for (int i = 0; i < taskCount; i++) {
                     System.out.println((i + 1) + ". " + tasks[i]);
                 }
+            } else if (input.startsWith("mark ")) {
+                int taskNumber = Integer.parseInt(input.substring(5));
+                tasks[taskNumber - 1].markAsDone();
+
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  " + tasks[taskNumber - 1]);
+            } else if (input.startsWith("unmark ")) {
+                int taskNumber = Integer.parseInt(input.substring(7));
+                tasks[taskNumber - 1].markAsNotDone();
+
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  " + tasks[taskNumber - 1]);
             } else {
-                tasks[taskCount] = input;
+                tasks[taskCount] = new Task(input);
                 taskCount++;
                 System.out.println("added: " + input);
             }
