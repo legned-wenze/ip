a977e2cadc0a9559f8d2ff9794bc6479e896c78a
lwz <lwz@123.com>
2026-08-21 15:24:45 +0800

    A-Enums

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,22 +1,24 @@
 public class Task {
     protected String description;
     protected boolean isDone;
+    protected TaskType type;
 
-    public Task(String description) {
+    public Task(String description, TaskType type) {
         this.description = description;
         this.isDone = false;
-    }
-
-    public void markAsDone() {
-        this.isDone = true;
-    }
-
-    public void markAsNotDone() {
-        this.isDone = false;
+        this.type = type;
     }
 
     public String getStatusIcon() {
         return isDone ? "X" : " ";
+    }
+
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    public void markAsNotDone() {
+        isDone = false;
     }
 
     @Override
