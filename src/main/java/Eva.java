1a4a0c4b72f0a20334f23441e55c3f00de19e721
lwz <lwz@123.com>
2026-08-21 14:15:46 +0800

    Level-2

--- src/main/java/Eva.java
+++ src/main/java/Eva.java
@@ -4,6 +4,9 @@ public class Eva {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         System.out.println("Hello! I'm Eva.");
         System.out.println("What can I do for you?");
 
@@ -13,9 +16,15 @@ public class Eva {
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            }else if (input.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("added: " + input);
             }
-
-            System.out.println(input);
         }
 
         scanner.close();
