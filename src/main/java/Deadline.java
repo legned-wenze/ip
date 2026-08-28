a4015087bf25f043cfbc8a4a6ee434dd08a838de
lwz <lwz@123.com>
2026-08-28 12:27:08 +0800

    Parse and format deadline dates

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,9 +1,23 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Locale;
+
 public class Deadline extends Task {
-    protected String by;
+    private static final DateTimeFormatter OUTPUT_DATE_FORMAT =
+            DateTimeFormatter.ofPattern("MMM dd yyyy", Locale.ENGLISH);
+
+    protected LocalDate by;
 
     public Deadline(String description, String by) {
         super(description, TaskType.DEADLINE);
-        this.by = by;
+
+        try {
+            this.by = LocalDate.parse(by);
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException(
+                    "The date must use yyyy-MM-dd format.");
+        }
     }
 
     @Override
@@ -13,6 +27,7 @@ public class Deadline extends Task {
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString()
+                + " (by: " + by.format(OUTPUT_DATE_FORMAT) + ")";
     }
 }
